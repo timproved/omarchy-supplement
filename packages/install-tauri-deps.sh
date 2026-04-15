7bd3ffe5d7752cd6ea377ace92a77e39b16589cc
timproved <48419301+timproved@users.noreply.github.com>
2026-04-15 08:21:12 +0200

    added tauri deps and deleted protonmail

--- packages/install-tauri-deps.sh
+++ packages/install-tauri-deps.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=packages/lib.sh
 source "$SCRIPT_DIR/lib.sh"
 
-install_aur_packages proton-mail
+install_arch_packages  webkit2gtk-4.1 base-devel curl wget file openssl appmenu-gtk-module libappindicator-gtk3 librsvg xdotool
